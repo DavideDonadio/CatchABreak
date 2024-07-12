916871bdbb26f0b03b7c343953c222eafb86b4df
Davide Donadio <davide.donadio@protonmail.com>
2024-07-12 16:33:58 +0200

    Added SceneManager to manage scene switching

---
+++ src/main/java/com/catchabreak/SceneManager.java
@@ -0,0 +1,38 @@
+package com.catchabreak;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SceneManager {
+
+    private Stage stage;
+    private String cssPath;
+
+    public SceneManager(Stage stage, String cssPath){
+
+        this.stage = stage;
+        this.cssPath = cssPath;
+    }
+
+    public void switchScene(String fxmlFileName) {
+
+        try {
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFileName + ".fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+
+            scene.getStylesheets().add(getClass().getResource(cssPath).toExternalForm());
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}

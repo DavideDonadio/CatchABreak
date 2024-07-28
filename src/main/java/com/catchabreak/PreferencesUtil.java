4e7ada80c629d9ac89b15cdda0b1a44a565891ee
Davide Donadio <davide.donadio@protonmail.com>
2024-07-28 22:53:04 +0200

    Changed variables name for readability

--- src/main/java/com/catchabreak/PreferencesUtil.java
+++ src/main/java/com/catchabreak/PreferencesUtil.java
@@ -7,12 +7,12 @@ public class PreferencesUtil {
     private static final Preferences prefs = Preferences.userNodeForPackage(PreferencesUtil.class);
 
     private static final String THEME_KEY = "THEME";
-    private static final String BREAKTIMEMINUTES_KEY = "BTM";
-    private static final String WORKTIMEMINUTES_KEY = "WTS";
+    private static final String BREAKTIME_MINUTES_KEY = "BTM";
+    private static final String WORKTIME_MINUTES_KEY = "WTS";
     private static final String NUMOFBREAKS_KEY = "BREAKS";
-    private static final String NUMOFTIMERSTARTED_KEY = "TIMERSTART";
-    private static final String NUMOFTIMERSTOPPED_KEY = "TIMERSTOP";
-    private static final String NUMOFTIMERRESTARTED_KEY = "TIMERESTART";
+    private static final String NUMOF_TIMERSTARTED_KEY = "TIMERSTART";
+    private static final String NUMOF_TIMERSTOPPED_KEY = "TIMERSTOP";
+    private static final String NUMOF_TIMER_RESTARTED_KEY = "TIMERESTART";
 
     public static String getTheme(){
         return prefs.get(THEME_KEY, "light");
@@ -23,27 +23,27 @@ public class PreferencesUtil {
     }
 
     public static int getBreakTimeMinutes(){
-        return prefs.getInt(BREAKTIMEMINUTES_KEY, 5);
+        return prefs.getInt(BREAKTIME_MINUTES_KEY, 5);
     }
 
     public static int getWorkTimeMinutes(){
-        return prefs.getInt(WORKTIMEMINUTES_KEY, 20);
+        return prefs.getInt(WORKTIME_MINUTES_KEY, 20);
     }
 
     public static int getWorkTimeSeconds(){
-        return (prefs.getInt(WORKTIMEMINUTES_KEY, 20) * 60);
+        return (prefs.getInt(WORKTIME_MINUTES_KEY, 20) * 60);
     }
 
     public static int getBreakTimeSeconds(){
-        return (prefs.getInt(BREAKTIMEMINUTES_KEY, 5) * 60);
+        return (prefs.getInt(BREAKTIME_MINUTES_KEY, 5) * 60);
     }
 
     public static void setBreakTimeMinutes(int minutes){
-        prefs.putInt(BREAKTIMEMINUTES_KEY, minutes);
+        prefs.putInt(BREAKTIME_MINUTES_KEY, minutes);
     }
 
     public static void setWorkTimeMinutes(int minutes){
-        prefs.putInt(WORKTIMEMINUTES_KEY, minutes);
+        prefs.putInt(WORKTIME_MINUTES_KEY, minutes);
     }
 
     public static int getNumberOfBreaks(){
@@ -55,27 +55,32 @@ public class PreferencesUtil {
     }
 
     public static int getNumberOfTimerStarted(){
-        return (prefs.getInt(NUMOFTIMERSTARTED_KEY, 0));
+        return (prefs.getInt(NUMOF_TIMERSTARTED_KEY, 0));
     }
 
     public static int getNumberOfTimerStopped(){
-        return (prefs.getInt(NUMOFTIMERSTOPPED_KEY, 0));
+        return (prefs.getInt(NUMOF_TIMERSTOPPED_KEY, 0));
     }
 
     public static int getNumberOfTimerRestarted(){
-        return (prefs.getInt(NUMOFTIMERRESTARTED_KEY, 0));
+        return (prefs.getInt(NUMOF_TIMER_RESTARTED_KEY, 0));
     }
 
     public static void addTimerStart(){
-        prefs.putInt(NUMOFTIMERSTARTED_KEY, getNumberOfTimerStarted() + 1);
+        prefs.putInt(NUMOF_TIMERSTARTED_KEY, getNumberOfTimerStarted() + 1);
     }   
 
     public static void addTimerStop(){
-        prefs.putInt(NUMOFTIMERSTOPPED_KEY, getNumberOfTimerStopped() + 1);
+        prefs.putInt(NUMOF_TIMERSTOPPED_KEY, getNumberOfTimerStopped() + 1);
     }   
 
     public static void addTimerRestart(){
-        prefs.putInt(NUMOFTIMERRESTARTED_KEY, getNumberOfTimerRestarted() + 1);
-    }   
+        prefs.putInt(NUMOF_TIMER_RESTARTED_KEY, getNumberOfTimerRestarted() + 1);
+    }
+
+    @SuppressWarnings("exports")
+    public static Preferences getPrefs() {
+        return prefs;
+    }
         
 }
